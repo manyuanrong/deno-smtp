6479643518d2e21084fa25d22efb8bdd0197740b
Tamas Szoke <tamas.szoke.dev@gmail.com>
2020-07-16 00:00:39 +0200

    Bump std versions for deno 1.2.0

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export {
   BufReader,
   BufWriter,
-} from "https://deno.land/std@v0.52.0/io/bufio.ts";
-export { TextProtoReader } from "https://deno.land/std@v0.52.0/textproto/mod.ts";
+} from "https://deno.land/std@v0.61.0/io/bufio.ts";
+export { TextProtoReader } from "https://deno.land/std@v0.61.0/textproto/mod.ts";
 export {
   format,
   setColorEnabled,
