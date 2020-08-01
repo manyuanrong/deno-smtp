7bed1d2cd1763d7f1915c7777521c224649a386b
Steven Guerrero <stephenguerrero43@gmail.com>
2020-07-31 20:36:50 -0500

    (feat) Add support for options in the SMTP client (#29)
    * Add support for options in the SMTP client
    
    * Bump to Deno v1.2.2

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export {
   BufReader,
   BufWriter,
-} from "https://deno.land/std@v0.60.0/io/bufio.ts";
-export { TextProtoReader } from "https://deno.land/std@v0.60.0/textproto/mod.ts";
+} from "https://deno.land/std@v0.63.0/io/bufio.ts";
+export { TextProtoReader } from "https://deno.land/std@v0.63.0/textproto/mod.ts";
