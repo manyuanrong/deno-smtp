6445448cc77ce971130c15c406043e74547a212e
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-10-05 12:28:10 -0500

    Fix 'v' version warnings

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export {
   BufReader,
   BufWriter,
-} from "https://deno.land/std@v0.73.0/io/bufio.ts";
-export { TextProtoReader } from "https://deno.land/std@v0.73.0/textproto/mod.ts";
+} from "https://deno.land/std@0.73.0/io/bufio.ts";
+export { TextProtoReader } from "https://deno.land/std@0.73.0/textproto/mod.ts";
